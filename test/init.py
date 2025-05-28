beb660293d88a301aabf32776ff343cc9600ddc5
Jakob Lass <lass.jakob@gmail.com>
2025-05-28 09:27:34 +0200

    Update Author check

--- test/init.py
+++ test/init.py
@@ -3,4 +3,4 @@ import DMCpy
 
 def test_initialization():
     assert(DMCpy.__version__=='1.0.1')
-    assert(DMCpy.__author__=='Jakob Lass')
+    assert(DMCpy.__author__=='Jakob Lass, Sam Moody, Øystein S. Fjellvåg')
